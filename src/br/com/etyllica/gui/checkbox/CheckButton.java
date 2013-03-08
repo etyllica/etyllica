221feac4e933b2a52f7614c803b207770fe33795
yuripourre <yuriamayuri@gmail.com>
2013-03-08 12:07:16 -0300

    CheckButton Component.

--- src/br/com/etyllica/gui/checkbox/CheckButton.java
+++ src/br/com/etyllica/gui/checkbox/CheckButton.java
@@ -1,4 +1,4 @@
-package br.com.etyllica.gui;
+package br.com.etyllica.gui.checkbox;
 
 import br.com.etyllica.core.Configuration;
 import br.com.etyllica.core.Theme;
@@ -7,6 +7,7 @@ import br.com.etyllica.core.event.GUIEvent;
 import br.com.etyllica.core.event.KeyState;
 import br.com.etyllica.core.event.PointerEvent;
 import br.com.etyllica.core.video.Grafico;
+import br.com.etyllica.gui.Button;
 
 /**
  * 
@@ -15,11 +16,11 @@ import br.com.etyllica.core.video.Grafico;
  *
  */
 
-public class CheckBox extends Button{
+public class CheckButton extends Button{
 
 	protected boolean checked = false;
 
-	public CheckBox(int x, int y) {
+	public CheckButton(int x, int y) {
 		super(x, y, 22, 22);
 	}
 
@@ -83,27 +84,11 @@ public class CheckBox extends Button{
 			}
 		}
 
-		g.fillRect(x,y,w,h);
-
-
-
 		if(checked){
-
-			//DrawShadow
-			if(theme.isShadow()){
-
-				g.setColor(theme.getShadowColor());
-				
-				g.drawLine(x+3,y+3,x+w/2+1, y+h/2+1);
-				g.drawLine(x+w/2+1, y+h/2+1,x+w+5+1,y-5+1);
-			}
-
-			g.setColor(theme.getTextColor());
-
-			g.drawLine(x+2,y+2,x+w/2, y+h/2);
-			g.drawLine(x+w/2, y+h/2,x+w+5,y-5);
-
+			g.setColor(theme.getButtonOnClick());
 		}
+		
+		g.fillRect(x,y,w,h);
 
 	}
 
