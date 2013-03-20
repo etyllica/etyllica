15911e509805df4b1df0e06584e4eb4edba73093
yuripourre <yuriamayuri@gmail.com>
2013-03-20 01:57:38 -0300

    Pivot drawing fix.

--- src/br/com/etyllica/layer/PivotImageLayer.java
+++ src/br/com/etyllica/layer/PivotImageLayer.java
@@ -1,5 +1,7 @@
 package br.com.etyllica.layer;
 
+import java.awt.geom.AffineTransform;
+
 import br.com.etyllica.core.loader.ImageLoader;
 import br.com.etyllica.core.video.Grafico;
 
@@ -12,49 +14,77 @@ import br.com.etyllica.core.video.Grafico;
 
 public class PivotImageLayer extends ImageLayer{
 
-	protected int xPivot;
-	protected int yPivot;
+	protected int xPivot = 0;
+	protected int yPivot = 0;
 	
 	public PivotImageLayer(int x, int y){
 		super(x,y);
-		
-		xPivot = 0;
-		yPivot = 0;
 	}
 	
 	public PivotImageLayer(int x, int y, String caminho){
 		super(x,y,caminho);
 		
-		xPivot = 0;
-		yPivot = 0;
-	}
-	
-	public PivotImageLayer(){
-		super();
-		
-		xPivot = 0;
-		yPivot = 0;
-	}
+		this.xPivot = w/2;
+		this.yPivot = h/2;
+	}	
 	
 	public void setPivotCoordinates(int xPivot,int yPivot){
 		this.xPivot = xPivot;
 		this.yPivot = yPivot;
 	}
+	
 	public int getXPivot(){
-		//return x+xPivot;
 		return xPivot;
 	}
+	
 	public int getYPivot(){
-		//return y+yPivot;
 		return yPivot;
 	}
 	
 	@Override
+	public boolean isPointInsideRectangle(int mx, int my){
+
+			//Pivot Point of rotation
+			int px = x+xPivot;
+			int py = y+yPivot;
+			
+			double c = Math.cos(angle);
+
+			double s = Math.sin(angle);
+
+			// UNrotate the point depending on the rotation of the rectangle
+			double rotatedX = px + c * (mx - px) - s * (my - py);
+
+			double rotatedY = py + s * (mx - px) + c * (my - py);
+
+			// perform a normal check if the new point is inside the 
+			// bounds of the UNrotated rectangle		
+			int leftX = px - w / 2;
+			int rightX = px + w/2;
+			int topY = py - h/2;
+			int bottomY = py + h/2;
+			
+			return (leftX <= rotatedX && rotatedX <= rightX && topY <= rotatedY && rotatedY <= bottomY);
+		}
+	
+	@Override	
 	public void draw(Grafico g){
 		if(visible){
-			g.drawImage( ImageLoader.getInstance().getImage(path), x, y, x+w,y+h,
-					xImage,yImage,xImage+w,yImage+h, null );
 
-		}		
+			if(angle==0){
+				g.drawImage( ImageLoader.getInstance().getImage(path), x, y, x+w, y+h,
+						xImage,yImage,xImage+w,yImage+h, null );
+			}else{
+
+				AffineTransform reset = g.getTransform();
+
+				AffineTransform transform = AffineTransform.getRotateInstance(Math.toRadians(angle),x+xPivot, y+yPivot);
+				
+				g.setTransform(transform);
+				g.drawImage( ImageLoader.getInstance().getImage(path), x, y, x+w, y+h,
+						xImage,yImage,xImage+w,yImage+h, null );
+				g.setTransform(reset);
+			}
+		}
 	}
 }
