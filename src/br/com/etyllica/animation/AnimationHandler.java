adc608e53a273d7435b4122ce8fbc5de3c4e8c2b
Yuri Pourre <yuriamayuri@gmail.com>
2013-07-24 01:42:03 -0300

    Complete Merge

--- src/br/com/etyllica/animation/AnimationHandler.java
+++ src/br/com/etyllica/animation/AnimationHandler.java
@@ -12,9 +12,9 @@ public class AnimationHandler {
 	private List<AnimationScript> scripts = new ArrayList<AnimationScript>();
 
 	private Set<AnimationScript> removeScripts = new HashSet<AnimationScript>();
-	
+
 	private Set<AnimationScript> nextScripts = new HashSet<AnimationScript>();
-	
+
 	private Map<AnimationScript, AnimationScript> endlessScripts = new HashMap<AnimationScript, AnimationScript>();
 
 	public AnimationHandler(){
@@ -30,25 +30,32 @@ public class AnimationHandler {
 			}else{
 				//if stopped, use child (next script)
 				if(script.getNext()!=null){
-					
+
 					if(script.isEndless()){
 						endlessScripts.put(lastScript(script),script);
 					}
-					
+
 					nextScripts.add(script.getNext());
-					
-					
+
+
 				}else{
-					
+
 					//If this script has some associated endless script
 					if(endlessScripts.containsKey(script)){
 						//Find and restart the endless script
 						AnimationScript endless = endlessScripts.get(script);
 						restartEndless(endless);
-						
+
 						nextScripts.add(endless);
+
+					}else{
+
+						//If script is endless and don't have next
+						if(script.isEndless()){
+							script.restart();
+							nextScripts.add(script);
+						}
 					}
-					
 				}
 				removeScripts.add(script);
 			}
@@ -61,12 +68,12 @@ public class AnimationHandler {
 		}
 
 		removeScripts.clear();
-		
+
 		//Add next Scripts
 		for(AnimationScript script: nextScripts){
 			scripts.add(script);
 		}
-		
+
 		nextScripts.clear();		
 
 	}
@@ -74,31 +81,31 @@ public class AnimationHandler {
 	public void add(AnimationScript script){
 		scripts.add(script);
 	}
-	
+
 	private AnimationScript restartEndless(AnimationScript script){
-		
+
 		script.restart();
-		
+
 		AnimationScript last = script;
-		
+
 		if(script.getNext()!=null){
 			last = restartEndless(script.getNext()); 
 		}
-		
+
 		return last;
-		
+
 	}
-	
+
 	private AnimationScript lastScript(AnimationScript script){
-		
+
 		AnimationScript last = script;
-		
+
 		if(script.getNext()!=null){
 			last = lastScript(script.getNext()); 
 		}
-		
+
 		return last;
-		
+
 	}
 
 }
