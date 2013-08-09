74a5a81b99d80617c9c773bdfe6fa1f1a4e488c5
Yuri Pourre <yuriamayuri@gmail.com>
2013-08-09 19:51:40 -0300

    Spiral Animation, renamed to Orbit Animation

--- src/br/com/etyllica/animation/scripts/OrbitAnimationScript.java
+++ src/br/com/etyllica/animation/scripts/OrbitAnimationScript.java
@@ -4,39 +4,39 @@ import br.com.etyllica.animation.AnimationScript;
 import br.com.etyllica.layer.Layer;
 
 
-public class SpiralAnimationScript extends AnimationScript{
+public class OrbitAnimationScript extends AnimationScript{
 
-	public SpiralAnimationScript(long time){
+	private int centerX = 0, centerY = 0;
+	private int tx, ty, tw, th;
+	
+	public OrbitAnimationScript(long time){
 		super(time);
 	}
 
-	public SpiralAnimationScript(long delay, long time){
+	public OrbitAnimationScript(long delay, long time){
 		super(delay, time);
 	}
 
-	public SpiralAnimationScript(Layer target, long time){
+	public OrbitAnimationScript(Layer target, long time){
 		super(target, time);
 	}
-	
-	int cx = 0, cy = 0;
-	
+		
 	public void setCenter(int centerX, int centerY){
-		this.cx = centerX;
-		this.cy = centerY;
+		this.centerX = centerX;
+		this.centerY = centerY;
 	}
-	
-	int tx, ty, tw, th;
-	
+				
+	@Override
 	public void setTarget(Layer target){
-		this.target = target;
+		super.setTarget(target);
+		
 		tx = target.getX();
 		ty = target.getY();
-		
 		tw = target.getW()/2;
 		th = target.getH()/2;
+				
 	}
-
-
+	
 	@Override
 	public void update(double value){
 
@@ -46,19 +46,16 @@ public class SpiralAnimationScript extends AnimationScript{
 		double c = Math.cos(angle);
 
 		// translate point back to origin:
-		int px = tx+tw;
-		int py = ty+th;
-
-		px -= cx;
-		py -= cy;
+		int px = tx+tw-centerX;
+		int py = ty+th-centerY;
 
 		// rotate point
 		double xnew = px * c - py * s;
 		double ynew = px * s + py * c;
 
 		// translate point back:
-		target.setX((int)xnew + cx - tw);
-		target.setY((int)ynew + cy - th);
+		target.setX((int)xnew + centerX - tw);
+		target.setY((int)ynew + centerY - th);
 
 	}	
 
