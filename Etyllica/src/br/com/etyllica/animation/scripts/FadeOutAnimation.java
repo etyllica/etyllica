81daf94b4397bb3e0f1e55b9862ab5e1b963e340
Yuri Pourre <yuriamayuri@gmail.com>
2013-12-15 13:50:21 -0200

    FadeOutAnimation +BugFix in FadeInAnimation

--- Etyllica/src/br/com/etyllica/animation/scripts/FadeOutAnimation.java
+++ Etyllica/src/br/com/etyllica/animation/scripts/FadeOutAnimation.java
@@ -3,24 +3,24 @@ package br.com.etyllica.animation.scripts;
 import br.com.etyllica.layer.Layer;
 
 
-public class FadeInAnimation extends OpacityAnimation{
+public class FadeOutAnimation extends OpacityAnimation{
 		
-	public FadeInAnimation(long time){
+	public FadeOutAnimation(long time){
 		super(time);
 		
-		startValue = 0;
-		endValue = 255;
+		startValue = 255;
+		endValue = 0;
 		
 	}
 	
-	public FadeInAnimation(long delay, long time){
+	public FadeOutAnimation(long delay, long time){
 		super(delay, time);
 		
 		startValue = 0;
 		endValue = 255;
 	}
 	
-	public FadeInAnimation(Layer target, long time){
+	public FadeOutAnimation(Layer target, long time){
 		super(target, time);
 		
 		startValue = 0;
@@ -30,7 +30,7 @@ public class FadeInAnimation extends OpacityAnimation{
 	@Override
 	public void setTarget(Layer target) {
 		super.setTarget(target);
-		target.setOpacity(0);
+		target.setOpacity(255);
 	}
 	
 	@Override
