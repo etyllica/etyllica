6057b199b145bb0bad6a790025d89ecf2adb10a7
Yuri Pourre <yuriamayuri@gmail.com>
2014-01-07 20:07:43 -0200

    Another machine in example

--- Etyllica/src/examples/etyllica/tutorial03/StriderAnimation.java
+++ Etyllica/src/examples/etyllica/tutorial03/StriderAnimation.java
@@ -14,22 +14,12 @@ public class StriderAnimation extends Application{
 	public StriderAnimation(int w, int h) {
 		super(w, h);
 	}
-
-	@Override
-	public GUIEvent updateMouse(PointerEvent event) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public GUIEvent updateKeyboard(KeyEvent event) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
 	private ImageLayer background;
 	
 	private ImageLayer machine;
+	
+	private ImageLayer anotherMachine;
 
 	@Override
 	public void load() {
@@ -38,20 +28,29 @@ public class StriderAnimation extends Application{
 		
 		background = new ImageLayer("scene.png");
 		machine = new ImageLayer(180,270,"machine.png");
+		anotherMachine = new ImageLayer(180,270,"machine.png");
+		
+		createAnimationScripts(machine, 0);
+		
+		createAnimationScripts(anotherMachine, 180);
 				
 		loading = 50;
 		loadingPhrase = "Loading animations...";		
+				
+	}
+	
+	private void createAnimationScripts(ImageLayer machine, int offsetAngle){
 		
 		OrbitAnimation orbitAnimation = new OrbitAnimation(0,5000);
 		orbitAnimation.setTarget(machine);
 		orbitAnimation.setCenter(machine.getX(), machine.getY()+70);
-		orbitAnimation.setInterval(0, 360);
+		orbitAnimation.setInterval(offsetAngle, 360+offsetAngle);
 		orbitAnimation.setEndless(true);
 		this.scene.addAnimation(orbitAnimation);
 		
 		RotateAnimation rotateAnimation = new RotateAnimation(0,5000);
 		rotateAnimation.setTarget(machine);
-		rotateAnimation.setInterval(0, 360);
+		rotateAnimation.setInterval(offsetAngle, 360+offsetAngle);
 		rotateAnimation.setEndless(true);
 		this.scene.addAnimation(rotateAnimation);
 		
@@ -61,6 +60,19 @@ public class StriderAnimation extends Application{
 	public void draw(Graphic g) {
 		background.draw(g);
 		machine.draw(g);
+		anotherMachine.draw(g);
+	}
+	
+	@Override
+	public GUIEvent updateMouse(PointerEvent event) {
+		// TODO Auto-generated method stub
+		return GUIEvent.NONE;
+	}
+
+	@Override
+	public GUIEvent updateKeyboard(KeyEvent event) {
+		// TODO Auto-generated method stub
+		return GUIEvent.NONE;
 	}
 
 }
