cc47e67e047b2834781466d51e5373738768950f
Yuri Pourre <yuriamayuri@gmail.com>
2014-05-03 15:06:03 -0300

    New node constructors

--- Etyllica/src/br/com/etyllica/linear/graph/Node.java
+++ Etyllica/src/br/com/etyllica/linear/graph/Node.java
@@ -3,9 +3,13 @@ package br.com.etyllica.linear.graph;
 import br.com.etyllica.linear.Point2D;
 
 public class Node extends Point2D {
-
-	public Node(int x, int y) {
+	
+	public Node(double x, double y) {
 		super(x, y);
+	}
+	
+	public Node(Point2D point) {
+		super(point.getX(), point.getY());
 	}
 	
 }
