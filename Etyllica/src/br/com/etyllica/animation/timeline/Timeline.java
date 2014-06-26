d380c36f19a5ba19179156070a0938e14dc9551c
Yuri Pourre <yuriamayuri@gmail.com>
2014-06-26 12:44:24 -0300

    Cursor in Timeline

--- Etyllica/src/br/com/etyllica/animation/timeline/Timeline.java
+++ Etyllica/src/br/com/etyllica/animation/timeline/Timeline.java
@@ -1,13 +1,40 @@
 package br.com.etyllica.animation.timeline;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Timeline<T> extends ArrayList<KeyFrame<T>> {
+public abstract class Timeline<T> {
 	
-	private static final long serialVersionUID = 4834676749729569640L;
-
+	protected int cursor = 0;
+	
+	protected List<KeyFrame<T>> frames = new ArrayList<KeyFrame<T>>();
+	
 	public Timeline() {
 		super();
 	}
+
+	public void nextFrame() {
+		
+		if(cursor<frames.size()-1) {
+			cursor++;
+			
+			reloadFrame(currentFrame());
+		}		
+	}
+	
+	public void previousFrame() {
+		
+		if(cursor > 0) {
+			cursor--;
+			
+			reloadFrame(currentFrame());
+		}
+	}
+	
+	public KeyFrame<T> currentFrame() {
+		return frames.get(cursor);
+	}
+	
+	public abstract void reloadFrame(KeyFrame<T> frame);
 	
 }
