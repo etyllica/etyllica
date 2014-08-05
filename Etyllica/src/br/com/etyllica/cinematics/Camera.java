f24c7844ca62e280fc103538831304030a39a401
Yuri Pourre <yuriamayuri@gmail.com>
2014-08-04 22:55:36 -0300

    Camera extends GeometricLayer

--- Etyllica/src/br/com/etyllica/cinematics/Camera.java
+++ Etyllica/src/br/com/etyllica/cinematics/Camera.java
@@ -2,9 +2,9 @@ package br.com.etyllica.cinematics;
 
 import java.awt.geom.AffineTransform;
 
-import br.com.etyllica.layer.Layer;
+import br.com.etyllica.layer.GeometricLayer;
 
-public class Camera extends Layer{
+public class Camera extends GeometricLayer {
 	
 	private float zoomX = 1;
 	private float zoomY = 1;
@@ -12,16 +12,16 @@ public class Camera extends Layer{
 	private float skewX = 0;
 	private float skewY = 0;
 	
-	public Camera(int x, int y, int w, int h){
+	public Camera(int x, int y, int w, int h) {
 		super(x, y, w, h);
 	}	
 	
-	public AffineTransform getTransform(){
+	public AffineTransform getTransform() {
 		
 		AffineTransform transform = AffineTransform.getScaleInstance(zoomX, zoomY);
 				
-		if(x!=0||y!=0){
-			transform.concatenate(AffineTransform.getTranslateInstance(-2*x, -2*y));
+		if(x != 0 || y != 0) {
+			transform.concatenate(AffineTransform.getTranslateInstance(-x, -y));
 		}
 
 		return transform;
