56f092ea475d1682bfdd144469506385a971dad9
Yuri Pourre <yuriamayuri@gmail.com>
2014-09-05 03:19:51 -0300

    Improvements in LightSource

--- Etyllica/src/br/com/etyllica/effects/light/LightSource.java
+++ Etyllica/src/br/com/etyllica/effects/light/LightSource.java
@@ -9,10 +9,20 @@ import br.com.etyllica.layer.GeometricLayer;
 
 public class LightSource extends GeometricLayer {
 		
+	private float[] fractions = {.2f,.8f};
+	
+	private Color[] colors = {new Color(0f, 0f, 0f, 0f), new Color(0f, 0f, 0f, 1f)};
+	
+	private RadialGradientPaint rgp;
+	
 	public LightSource(int x, int y, int intensity) {
 		super(x, y);
 		w = intensity;
 		h = intensity;
+		
+		java.awt.geom.Point2D center = new java.awt.geom.Point2D.Float(x+w/2, y+h/2);
+		
+		rgp = new RadialGradientPaint(center, w/2, fractions, colors);
 	}
 	
 	public int getIntensity() {
@@ -22,12 +32,7 @@ public class LightSource extends GeometricLayer {
 	public void drawLight(Graphic g) {
 
 		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_IN, 1.0f));
-        		
-		float[] fractions = {.2f,.8f};
-		Color[] colors = {new Color(0f, 0f, 0f, 0f), new Color(0f, 0f, 0f, 1f)};
-        java.awt.geom.Point2D center = new java.awt.geom.Point2D.Float(x+w/2, y+h/2);
-        
-        RadialGradientPaint rgp = new RadialGradientPaint(center, w/2, fractions, colors);
+                        
         g.setPaint(rgp);
         
         g.fillOval(x, y, w, h);
