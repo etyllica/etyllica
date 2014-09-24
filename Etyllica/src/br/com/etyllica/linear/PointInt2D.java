597f22c53bf1815cbb521edbd53dd48b7fe7fe2a
Yuri Pourre <yuriamayuri@gmail.com>
2014-09-24 19:41:45 -0300

    Add PointInt2D class

---
+++ Etyllica/src/br/com/etyllica/linear/PointInt2D.java
@@ -0,0 +1,41 @@
+package br.com.etyllica.linear;
+
+public class PointInt2D {
+
+	private int x = 0;
+	
+	private int y = 0;
+	
+	public PointInt2D() {
+		super();
+	}
+	
+	public PointInt2D(int x, int y) {
+		super();
+		
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setLocation(int x, int y) {
+		setX(x);
+		setY(y);
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+		
+}
