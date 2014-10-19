31d956dcf4ecfc743f8a407e9198e82cc32c4850
Yuri Pourre <yuriamayuri@gmail.com>
2014-10-19 05:07:49 -0200

    Improves SimpleGuiExample

--- Etyllica/src/examples/etyllica/tutorial09/application/SimpleGuiExample.java
+++ Etyllica/src/examples/etyllica/tutorial09/application/SimpleGuiExample.java
@@ -5,8 +5,6 @@ import java.awt.Color;
 import br.com.etyllica.context.Application;
 import br.com.etyllica.core.event.Action;
 import br.com.etyllica.core.event.GUIEvent;
-import br.com.etyllica.core.event.KeyEvent;
-import br.com.etyllica.core.event.PointerEvent;
 import br.com.etyllica.core.graphics.Graphic;
 import br.com.etyllica.core.graphics.SVGColor;
 import br.com.etyllica.gui.Button;
@@ -28,83 +26,50 @@ public class SimpleGuiExample extends Application{
 		
 		Button buttonWhite = new Button(20,30,120,40);
 		buttonWhite.setLabel(new TextLabel("WHITE!"));
-		buttonWhite.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "turnWhite"));
+		buttonWhite.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "changeColor", SVGColor.GHOST_WHITE));
 		add(buttonWhite);		
 		
 		Button buttonBlue = new Button(20,80,120,40);
 		buttonBlue.setLabel(new TextLabel("BLUE!"));
-		buttonBlue.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "turnBlue"));
+		buttonBlue.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "changeColor", SVGColor.ROYAL_BLUE));
 		add(buttonBlue);
 		
 		Button buttonSeaGreen = new Button(20,130,120,40);
 		buttonSeaGreen.setLabel(new TextLabel("SEA GREEN!"));
-		buttonSeaGreen.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "turnSeaGreen"));
+		buttonSeaGreen.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "changeColor", SVGColor.SEA_GREEN));
 		add(buttonSeaGreen);
 		
 		Button buttonOrchid = new Button(20,180,120,40);
 		buttonOrchid.setRoundness(10);
 		buttonOrchid.setLabel(new TextLabel("ORCHID!"));
-		buttonOrchid.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "turnOrchid"));
+		buttonOrchid.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "changeColor", SVGColor.ORCHID));
 		add(buttonOrchid);
 		
 		Button buttonOrange = new Button(20,230,120,40);
 		buttonOrange.setRoundness(10);
 		buttonOrange.setLabel(new TextLabel("ORANGE!"));
-		buttonOrange.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "turnOrange"));
+		buttonOrange.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "changeColor", SVGColor.ORANGE));
 		add(buttonOrange);
 		
 		Button buttonCrimson = new Button(20,280,120,40);
 		buttonCrimson.setRoundness(10);
 		buttonCrimson.setLabel(new TextLabel("CRIMSON!"));
-		buttonCrimson.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "turnCrimson"));
+		buttonCrimson.addAction(GUIEvent.MOUSE_LEFT_BUTTON_UP, new Action(this, "changeColor", SVGColor.CRIMSON));
 		add(buttonCrimson);
 		
 		loading = 100;
 	}
 	
-	public void turnWhite(){
-		color = Color.WHITE;
+	public void changeColor(Color color){
+		this.color = color;
 	}
 	
-	public void turnBlue(){
-		color = Color.BLUE;
-	}
-	
-	public void turnSeaGreen(){
-		color = SVGColor.LIGHT_SEA_GREEN;
-	}
-	
-	public void turnOrchid(){
-		color = SVGColor.ORCHID;
-	}
-	
-	public void turnOrange(){
-		color = SVGColor.ORANGE;
-	}
-	
-	public void turnCrimson(){
-		color = SVGColor.CRIMSON;
-	}
-
 	@Override
 	public void draw(Graphic g) {
 		
 		g.setColor(color);
 		g.fillRect(x, y, w, h);
 		
-	}
-
-	@Override
-	public GUIEvent updateMouse(PointerEvent event) {
-		// TODO Auto-generated method stub
-		return GUIEvent	.NONE;
-	}
-
-	@Override
-	public GUIEvent updateKeyboard(KeyEvent event) {
-				
-		// TODO Auto-generated method stub
-		return GUIEvent	.NONE;
 	}
 
 }
