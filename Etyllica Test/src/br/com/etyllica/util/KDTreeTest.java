96c42bf886649de86cf3d96d3b491f1b08811a7e
Yuri Pourre <yuriamayuri@gmail.com>
2014-11-06 00:35:47 -0200

    Add K-D Tree based on Simon D. Levy's code, found at http://home.wlu.edu/~levys/software/kd/

---
+++ Etyllica Test/src/br/com/etyllica/util/KDTreeTest.java
@@ -0,0 +1,103 @@
+package br.com.etyllica.util;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.etyllica.util.kdtree.KDTree;
+import br.com.etyllica.util.kdtree.KeyMissingException;
+import br.com.etyllica.util.kdtree.KeySizeException;
+
+/**
+ * Tests based on kddemo.java examples
+ * 
+ */
+public class KDTreeTest {
+
+	private double [] A = {2, 5};
+	private double [] B = {1, 1};
+	private double [] C = {3, 9};
+	
+	private KDTree<String> tree;
+	
+	
+	@Before
+	public void setUp() {
+		
+		final int DIMENSION = 2; //X and Y
+		
+		tree = new KDTree<String>(DIMENSION);		
+	}
+	
+	@Test
+	public void testInsertion() {
+		
+		insertValues();
+		
+		Assert.assertEquals(3, tree.size());
+				
+	}
+	
+	public void testSearchValue() {
+		
+		insertValues();
+		
+		try {
+			Assert.assertEquals("B", tree.search(B));
+		} catch (KeySizeException e) {
+			Assert.fail();
+		}
+		
+	}
+	
+	@Test
+	public void testSearchNearest() {
+
+		insertValues();
+		
+		double [] T = {1, 10};
+		
+		try {
+			Assert.assertEquals("C", tree.nearest(T));
+		} catch (KeySizeException e) {
+			Assert.fail();
+		}
+		
+	}
+	
+	@Test
+	public void testRemove() {
+		
+		insertValues();
+		
+		try {
+			tree.delete(C);
+		} catch (KeySizeException e) {
+			Assert.fail();
+		} catch (KeyMissingException e) {
+			Assert.fail();
+		}
+		
+		double [] T = {1, 10};
+		
+		try {
+			Assert.assertEquals("A", tree.nearest(T));
+		} catch (KeySizeException e) {
+			Assert.fail();
+		}
+		
+	}
+	
+	private void insertValues() {
+		
+		try {
+		    tree.insert(A, new String("A"));
+		    tree.insert(B, new String("B"));
+		    tree.insert(C, new String("C"));
+		} catch (Exception e) {
+		    Assert.fail();
+		}
+		
+	}
+	
+}
