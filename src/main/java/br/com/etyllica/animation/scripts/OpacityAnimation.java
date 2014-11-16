2ad424d063b0517c60ac567427e9a62192890ff1
Yuri Pourre <yuriamayuri@gmail.com>
2014-11-16 17:15:52 -0200

    Add default opacity interval

--- src/main/java/br/com/etyllica/animation/scripts/OpacityAnimation.java
+++ src/main/java/br/com/etyllica/animation/scripts/OpacityAnimation.java
@@ -7,18 +7,22 @@ public class OpacityAnimation extends SingleIntervalAnimation {
 		
 	public OpacityAnimation(long time) {
 		super(time);
+		setInterval(0, 255);
 	}
 	
 	public OpacityAnimation(long delay, long time) {
 		super(delay, time);
+		setInterval(0, 255);
 	}
 	
 	public OpacityAnimation(Layer target, long time) {
 		super(target, time);
+		setInterval(0, 255);
 	}
 	
 	public OpacityAnimation(Layer target, long delay, long time) {
 		super(target, delay, time);
+		setInterval(0, 255);
 	}
 	
 	@Override
