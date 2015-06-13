9bd9ca937ec0a86f0eb4c354d5adeaa610de097e
yuripourre <yuriamayuri@gmail.com>
2015-06-13 19:27:10 -0400

    Improves Graph

--- src/main/java/br/com/etyllica/linear/graph/GenericComplexGraph.java
+++ src/main/java/br/com/etyllica/linear/graph/GenericComplexGraph.java
@@ -11,16 +11,17 @@ import br.com.etyllica.linear.Point2D;
 
 public abstract class GenericComplexGraph<N, E extends GenericEdge<N>> {
 
+	protected Set<E> edges;
 	protected Set<Node<N>> nodes;
-	
-	protected Map<Node<N>, List<E>> edges;
-	
+
+	protected Map<Node<N>, List<E>> edgesByNodes;
+
 	public GenericComplexGraph() {
 		super();
-		
+
 		nodes = new LinkedHashSet<Node<N>>();
-		
-		edges = new HashMap<Node<N>, List<E>>();
+		edges = new LinkedHashSet<E>();
+		edgesByNodes = new HashMap<Node<N>, List<E>>();
 	}
 
 	public Set<Node<N>> getNodes() {
@@ -34,85 +35,105 @@ public abstract class GenericComplexGraph<N, E extends GenericEdge<N>> {
 	public void addNode(Node<N> node) {
 		this.nodes.add(node);
 	}
-	
+
 	public void addNode(Point2D point) {
 		Node<N> node = new Node<N>();
 		node.setLocation(point.getX(), point.getY());
-		
+
 		nodes.add(node);		
 	}
 
 	public Map<Node<N>, List<E>> getAllEdges() {
+		return edgesByNodes;
+	}
+
+	public List<E> getAllEdgesAsList() {
+
+		List<E> list = new ArrayList<E>();
+		for(List<E> nodeEdges : edgesByNodes.values()) {
+			list.addAll(nodeEdges);
+		}
+
+		return list;
+	}
+
+	public Set<E> getEdges() {
 		return edges;
 	}
-	
+
 	public List<E> getEdges(Node<N> node) {
-		
-		if(edges.containsKey(node)) {
-			return edges.get(node);
+
+		if(edgesByNodes.containsKey(node)) {
+			return edgesByNodes.get(node);
 		}
-		
+
 		return new ArrayList<E>();
 	}
-	
+
 	public void addEdge(E edge) {
-		
+
+		edges.add(edge);
 		addNodesFromEdge(edge);
-		
-		Node<N> origin = edge.getOrigin();		
-		
-		if(!edges.containsKey(origin)) {
-			edges.put(origin, new ArrayList<E>());
+
+		Node<N> origin = edge.getOrigin();
+
+		if(!edgesByNodes.containsKey(origin)) {
+			edgesByNodes.put(origin, new ArrayList<E>());
 		}
-		
-		edges.get(origin).add(0, edge);
-				
+
+		List<E> originEdges = edgesByNodes.get(origin);
+		if (!originEdges.contains(edge)) {
+			edgesByNodes.get(origin).add(0, edge);
+		}
 	}
-	
+
 	private void addNodesFromEdge(GenericEdge<N> edge) {
-		
+
 		Node<N> origin = edge.getOrigin();
 
 		if(!nodes.contains(origin)) {
 			nodes.add(origin);
 		}
-		
+
 		Node<N> destination = edge.getDestination();
-		
+
 		if(!nodes.contains(destination)) {
 			nodes.add(destination);
 		}
-		
 	}
-	
+
 	public boolean hasDiretionalEdge(Node<N> origin, Node<N> destination) {
-		for(List<E> list: edges.values()) {
-			for(E edge: list) {
-				if(edge.getOrigin() == origin && edge.getDestination() == destination) {
-					return true;
-				}
+		for(E edge: edges) {
+			if(edge.getOrigin() == origin && edge.getDestination() == destination) {
+				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	public boolean hasEdge(Node<N> origin, Node<N> destination) {
-		for(List<E> list: edges.values()) {
-			for(E edge: list) {
-				if(edge.getOrigin() == origin && edge.getDestination() == destination) {
-					return true;
-				}
-				if(edge.getDestination() == origin && edge.getOrigin() == destination) {
-					return true;
-				}
+
+		for(E edge: edges) {
+			if(edge.getOrigin() == origin && edge.getDestination() == destination) {
+				return true;
+			}
+			if(edge.getDestination() == origin && edge.getOrigin() == destination) {
+				return true;
 			}
 		}
 		return false;
 	}
-	
+
+	public void mergeGraph(GenericComplexGraph<N, E> anotherGraph) {
+		for(E edge : anotherGraph.edges) {
+			addEdge(edge);	
+		}
+	}
+
 	public void clear() {
 		nodes.clear();
 		edges.clear();
+		edgesByNodes.clear();
 	}
-	
+
 }
