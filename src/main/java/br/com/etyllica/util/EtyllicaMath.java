2f4b91a4d16ea6a31a887042c5c84a223cc8b637
yuripourre <yuriamayuri@gmail.com>
2015-06-17 18:49:53 -0400

    Add Etyllica Math

---
+++ src/main/java/br/com/etyllica/util/EtyllicaMath.java
@@ -0,0 +1,16 @@
+package br.com.etyllica.util;
+
+public class EtyllicaMath {
+
+	public static double diffMod(double a, double b) {
+		
+		double diff = a-b;
+		
+		if(diff<0) {
+			diff = -diff;
+		}
+		
+		return diff;
+	}
+	
+}
