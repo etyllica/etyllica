59dd2f65d93d476d3a64c31f1811d15b72f55015
Yuri Pourre <yuriamayuri@gmail.com>
2015-07-07 11:21:26 -0400

    Improves CameraGL

--- src/main/java/br/com/abby/util/CameraGL.java
+++ src/main/java/br/com/abby/util/CameraGL.java
@@ -1,6 +1,6 @@
 package br.com.abby.util;
 
-import br.com.abby.linear.ColoredPoint3D;
+import br.com.etyllica.linear.Point3D;
 
 /**
  * 
@@ -9,27 +9,38 @@ import br.com.abby.linear.ColoredPoint3D;
  *
  */
 
-public class CameraGL extends ColoredPoint3D {
+public class CameraGL extends Point3D {
 	
-	private ColoredPoint3D target;
+	private Point3D target;
+	
+	private Point3D normal;
 	
 	public CameraGL(double x, double y, double z) {
 		super(x, y, z);
-		target = new ColoredPoint3D(0,0,0);
+		target = new Point3D(0,0,0);
+		normal = new Point3D(0,1,0);
 	}
 	
-	public ColoredPoint3D getTarget(){
+	public Point3D getTarget() {
 		return target;
 	}
-	
-	public void setTarget(ColoredPoint3D target){
+		
+	public void setTarget(Point3D target) {
 		this.target = target;
 	}
-	
-	public void setTarget(double x, double y, double z){
+		
+	public void setTarget(double x, double y, double z) {
 		target.setCoordinates(x, y, z);
-	}	
-	
+	}
+		
+	public Point3D getNormal() {
+		return normal;
+	}
+
+	public void setNormal(Point3D normal) {
+		this.normal = normal;
+	}
+
 	public double angleXY() {
 		return angle(x, target.getX(), y, target.getY());
 	}
