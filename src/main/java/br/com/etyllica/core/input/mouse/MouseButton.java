0c76aedc71537b8e86a01f8f91ed344fc8494bab
Yuri Pourre <yuriamayuri@gmail.com>
2015-07-14 16:12:29 -0400

    Remove awt dependecy from MouseButton

--- src/main/java/br/com/etyllica/core/input/mouse/MouseButton.java
+++ src/main/java/br/com/etyllica/core/input/mouse/MouseButton.java
@@ -1,24 +1,26 @@
 package br.com.etyllica.core.input.mouse;
 
-import java.awt.event.MouseEvent;
 
+/**
+ * Reference: http://docs.oracle.com/javase/7/docs/api/java/awt/event/MouseEvent.html
+ */
 public enum MouseButton {
 
-	MOUSE_BUTTON_LEFT(MouseEvent.BUTTON1),
-	MOUSE_BUTTON_MIDDLE(MouseEvent.BUTTON2),
-	MOUSE_BUTTON_RIGHT(MouseEvent.BUTTON3),	
-	MOUSE_WHEEL_UP(MouseEvent.MOUSE_WHEEL),
-	MOUSE_WHEEL_DOWN(MouseEvent.MOUSE_WHEEL+1),
+	MOUSE_BUTTON_LEFT(1),
+	MOUSE_BUTTON_MIDDLE(2),
+	MOUSE_BUTTON_RIGHT(3),	
+	MOUSE_WHEEL_UP(507),
+	MOUSE_WHEEL_DOWN(508),		
 	
 	MOUSE_NONE(0);	
 	
 	private final int code;
 
-	MouseButton(int code){
+	MouseButton(int code) {
 		this.code = code;
 	}
 	
-	public final int getCode(){ 
+	public final int getCode() { 
 		return code; 
 	}
 	
