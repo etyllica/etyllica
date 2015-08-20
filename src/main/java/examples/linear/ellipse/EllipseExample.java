50afff124e38b0e57e6f88ef72103eef4f3f4e39
Yuri Pourre <yuriamayuri@gmail.com>
2015-08-20 16:40:21 -0400

    Add Ellipse w/ drawing example

---
+++ src/main/java/examples/linear/ellipse/EllipseExample.java
@@ -0,0 +1,26 @@
+package examples.linear.ellipse;
+
+import br.com.etyllica.EtyllicaFrame;
+import br.com.etyllica.core.context.Application;
+import examples.linear.arrow.application.ArrowApplication;
+import examples.linear.ellipse.application.EllipseDrawing;
+
+public class EllipseExample extends EtyllicaFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public EllipseExample() {
+		super(640, 480);
+	}
+	
+	public static void main(String[] args){
+		EllipseExample example = new EllipseExample();
+		example.init();
+	}
+	
+	@Override
+	public Application startApplication() {
+		return new EllipseDrawing(w,h);
+	}
+	
+}
