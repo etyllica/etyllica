3ed7105fbafd0f8df8ee6444debafe0dcb2afac4
Yuri Pourre <yuriamayuri@gmail.com>
2016-01-11 14:01:58 -0200

    Override equals method in PointInt2D

--- src/main/java/br/com/etyllica/core/linear/PointInt2D.java
+++ src/main/java/br/com/etyllica/core/linear/PointInt2D.java
@@ -41,4 +41,29 @@ public class PointInt2D {
 		return "("+x+", "+y+")";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PointInt2D other = (PointInt2D) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+	
 }
