5bffd1466a0df64053dc9528593dddeca016557c
Yuri Pourre <yuripourre@gmail.com>
2016-03-17 11:11:05 -0300

    Add equals and hashCode to Point3D

--- src/main/java/br/com/etyllica/core/linear/Point3D.java
+++ src/main/java/br/com/etyllica/core/linear/Point3D.java
@@ -168,9 +168,41 @@ public class Point3D {
 		return new Point3D(x-p.x, y-p.y, z-p.z);
 	}
 	
+	@Override
 	public String toString() {
 		String text = "("+x+", "+y+", "+z+")"; 
 		return text;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(z);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point3D other = (Point3D) obj;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+			return false;
+		if (Double.doubleToLongBits(z) != Double.doubleToLongBits(other.z))
+			return false;
+		return true;
+	}
 }
