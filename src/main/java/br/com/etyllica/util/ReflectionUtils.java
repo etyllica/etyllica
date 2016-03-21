491958780763a4f8d22d82a093a455b5e5dc0f46
Yuri Pourre <yuriamayuri@gmail.com>
2016-03-21 11:31:49 -0300

    Add get and set methods to ReflectionUtils

--- src/main/java/br/com/etyllica/util/ReflectionUtils.java
+++ src/main/java/br/com/etyllica/util/ReflectionUtils.java
@@ -1,7 +1,58 @@
 package br.com.etyllica.util;
 
+import java.lang.reflect.Field;
+
 public class ReflectionUtils {
 
+	/**
+	 * Equivalent to set method 
+	 * Based on answer at: http://stackoverflow.com/a/14374995
+	 * @param object
+	 * @param fieldName
+	 * @param fieldValue
+	 * @return success
+	 */
+	public static boolean set(Object object, String fieldName, Object fieldValue) {
+	    Class<?> clazz = object.getClass();
+	    while (clazz != null) {
+	        try {
+	            Field field = clazz.getDeclaredField(fieldName);
+	            field.setAccessible(true);
+	            field.set(object, fieldValue);
+	            return true;
+	        } catch (NoSuchFieldException e) {
+	            clazz = clazz.getSuperclass();
+	        } catch (Exception e) {
+	            throw new IllegalStateException(e);
+	        }
+	    }
+	    return false;
+	}
+	
+	/**
+	 * Equivalent to get method 
+	 * Based on answer at: http://stackoverflow.com/a/14374995
+	 * @param object
+	 * @param fieldName
+	 * @return field value
+	 */
+	@SuppressWarnings("unchecked")
+	public static <V> V get(Object object, String fieldName) {
+	    Class<?> clazz = object.getClass();
+	    while (clazz != null) {
+	        try {
+	            Field field = clazz.getDeclaredField(fieldName);
+	            field.setAccessible(true);
+	            return (V) field.get(object);
+	        } catch (NoSuchFieldException e) {
+	            clazz = clazz.getSuperclass();
+	        } catch (Exception e) {
+	            throw new IllegalStateException(e);
+	        }
+	    }
+	    return null;
+	}
+	
 	public static Class<?> getArrayClass(Class<?> clazz) {
 		Class<?> arrayClass = clazz;
 		
@@ -13,5 +64,6 @@ public class ReflectionUtils {
 		
 		return arrayClass;
 	}
+	
 	
 }
