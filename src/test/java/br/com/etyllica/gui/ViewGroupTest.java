ef46efef5f1bdde81a688a24a9e49ffd1d5166de
Yuri Pourre <yuriamayuri@gmail.com>
2016-07-07 12:24:33 -0300

    Update test

--- src/test/java/br/com/etyllica/gui/ViewGroupTest.java
+++ src/test/java/br/com/etyllica/gui/ViewGroupTest.java
@@ -7,21 +7,25 @@ public class ViewGroupTest {
 
 	@Test
 	public void testAddViewInVerticalPanel() {
-		Panel panel = new Panel(0,0,100,50);
+		Panel panel = new Panel(0,0,260,360);
 		panel.setOrientation(Orientation.VERTICAL);
 		
 		TextView hello = new TextView("Hello");
 		panel.add(hello);
+		
+		//Round should be zero?
+		int round = 1;
+		
 		Assert.assertEquals(panel.getX(), hello.getX());
 		Assert.assertEquals(panel.getY(), hello.getY());
-		Assert.assertEquals(panel.getW(), hello.getW());
-		Assert.assertEquals(panel.getH(), hello.getH());
+		Assert.assertEquals(panel.getW()-round, hello.getW());
+		Assert.assertEquals(panel.getH()-round, hello.getH());
 		
 		TextView hi = new TextView("Hi!");
 		panel.add(hi);
 		
 		Assert.assertEquals(panel.getH()/2, hello.getH());
-		Assert.assertEquals(panel.getH()/2, hi.getH());
+		Assert.assertEquals(panel.getH()/2-round, hi.getH());
 	}
 	
 }
