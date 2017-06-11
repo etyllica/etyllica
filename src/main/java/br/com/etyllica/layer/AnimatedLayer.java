0b724e180a8a33001450c76878ba3e974de62fe0
Yuri Pourre <yuriamayuri@gmail.com>
2017-06-11 15:05:47 -0300

    Improves AnimateLayer

--- src/main/java/br/com/etyllica/layer/AnimatedLayer.java
+++ src/main/java/br/com/etyllica/layer/AnimatedLayer.java
@@ -4,326 +4,323 @@ import br.com.etyllica.commons.animation.OnAnimationFinishListener;
 import br.com.etyllica.commons.animation.OnFrameChangeListener;
 
 /**
- * 
  * @author yuripourre
- *
  */
 
 public class AnimatedLayer extends ImageLayer {
 
-	protected int tileW = 0;
-	
-	protected int tileH = 0;
-	
-	protected int needleX = 0;
-	
-	protected int needleY = 0;
+    protected int tileW = 0;
 
-	protected boolean once = false;
-	
-	protected boolean stopped = true;
-		
-	protected boolean animateHorizontally = true;
+    protected int tileH = 0;
 
-	protected boolean lockOnce = false;
+    protected int needleX = 0;
 
-	private int inc = 1;
+    protected int needleY = 0;
 
-	protected int frames = 1;
-	
-	protected int currentFrame = 0;
+    protected boolean once = false;
 
-	protected int speed = 500;
-	
-	protected long startedAt = 0;
-	
-	protected long changedAt = 0;
+    protected boolean stopped = true;
 
-	protected OnAnimationFinishListener onAnimationFinishListener;
-	
-	protected OnFrameChangeListener onFrameChangeListener;
-	
-	/**
-	 * 
-	 * @param x
-	 * @param y
-	 */
-	public AnimatedLayer(int x, int y) {
-		this(x, y, 0, 0);
-	}
+    protected boolean animateHorizontally = true;
 
-	/**
-	 * 
-	 * @param x
-	 * @param y
-	 * @param tileW
-	 * @param tileH
-	 * @param path
-	 */
-	public AnimatedLayer(int x, int y, int tileW, int tileH, String path) {
-		super(x,y,path);
-		
-		this.tileW = tileW;
-		this.tileH = tileH;		
-	}
+    protected boolean lockOnce = false;
 
-	/**
-	 * 
-	 * @param x
-	 * @param y
-	 * @param tileW
-	 * @param tileH
-	 */
-	public AnimatedLayer(int x, int y, int tileW, int tileH) {
-		super(x,y);
-		
-		this.tileW = tileW;
-		this.tileH = tileH;		
-	}
+    private int inc = 1;
 
-	public void restartAnimation() {
-		stopped = false;
-		resetAnimation();
-	}
-	
-	public void resetAnimation() {
-		xImage = needleX;
-		yImage = needleY;
-		currentFrame = 0;
-	}
+    protected int frames = 1;
 
-	public void setAnimateHorizontally(boolean animateHorizontally) {
-		this.animateHorizontally = animateHorizontally;
-	}
+    protected int currentFrame = 0;
 
-	public int getTileW() {
-		return tileW;
-	}
+    protected int speed = 500;
 
-	public int getTileH() {
-		return tileH;
-	}
+    protected long startedAt = 0;
 
-	/**
-	 * 
-	 * @param tileW
-	 */
-	public void setTileW(int tileW) {
-		this.tileW = tileW;
-	}
+    protected long changedAt = 0;
 
-	/**
-	 * 
-	 * @param tileH
-	 */
-	public void setTileH(int tileH) {
-		this.tileH = tileH;
-	}
+    protected OnAnimationFinishListener onAnimationFinishListener = DUMMY_ANIMATION_FINISH_LISTENER;
 
-	/**
-	 * 
-	 * @param tileW
-	 * @param tileH
-	 */
-	public void setTileCoordinates(int tileW, int tileH) {
-		setTileW(tileW);
-		setTileH(tileH);
-	}
+    protected OnFrameChangeListener onFrameChangeListener = DUMMY_FRAME_CHANGE_LISTENER;
 
-	public void animateWithFrame(int frame) {
-		if(this.currentFrame != frame) {
-			notifyFrameChangeListener(0, frame);	
-		}
-		
-		setFrame(frame);
-		
-		if (frame == frames-1) {
-			notifyAnimationFinishListener(0);
-		}
-	}	
-	
-	public void animate(long now) {
-		
-		if(stopped) {
-			
-			startedAt = now;
-			changedAt = now;
-		
-			restartAnimation();
-		}
-	
-		if(now >= changedAt+speed) {
-			
-			changedAt = now;
-			
-			boolean hasNextFrame = nextFrame();
-			
-			notifyFrameChangeListener(now, currentFrame);
-			
-			if(!hasNextFrame) {
-				notifyAnimationFinishListener(now);
-			}
-						
-		}
-		
-	}
-		
-	//Notify Listener about the end of animation
-	protected void notifyAnimationFinishListener(long now) {
-		
-		if(onAnimationFinishListener != null) {
-			onAnimationFinishListener.onAnimationFinish(now);
-		}
-		
-	}
-	
-	//Notify Listener about the frame change
-	protected void notifyFrameChangeListener(long now, int currentFrame) {
-		if(onFrameChangeListener != null) {
-			onFrameChangeListener.onFrameChange(now, currentFrame);
-		}
-	}
-	
-	public void animate() {
-		nextFrame();
-		stopped = false;
-	}
+    private static final OnAnimationFinishListener DUMMY_ANIMATION_FINISH_LISTENER = new OnAnimationFinishListener() {
+        @Override
+        public void onAnimationFinish(long now) {
+        }
+    };
 
-	public void stopAnimation() {
-		stopped = true;
-	}	
+    private static final OnFrameChangeListener DUMMY_FRAME_CHANGE_LISTENER = new OnFrameChangeListener() {
+        @Override
+        public void onFrameChange(long now, int currentFrame) {
+        }
+    };
 
-	public void animateOnce() {
-		once = true;
-		stopped = false;
-		lockOnce = false;
+    /**
+     * @param x
+     * @param y
+     */
+    public AnimatedLayer(int x, int y) {
+        this(x, y, 0, 0);
+    }
 
-		currentFrame = 0;
-		setFrame(currentFrame);
-	}
-	
-	public boolean nextFrame() {
-		
-		boolean hasNextFrame = true;
-		
-		if((currentFrame < frames-1) && (currentFrame >= 0)) {
-		
-			currentFrame+=inc;
-			
-		} else {
+    /**
+     * @param x
+     * @param y
+     * @param tileW
+     * @param tileH
+     * @param path
+     */
+    public AnimatedLayer(int x, int y, int tileW, int tileH, String path) {
+        super(x, y, path);
 
-			if(once) {
-				lockOnce = true;
-				//stopped = true;
-				//setFrame(currentFrame);
-				
-			} else {
-				currentFrame = 0;
-			}
-			
-			hasNextFrame = false;
-						
-		}
+        this.tileW = tileW;
+        this.tileH = tileH;
+    }
 
-		if(!stopped) {
-			setFrame(currentFrame);
-		}
-		
-		return hasNextFrame;
-	}
+    /**
+     * @param x
+     * @param y
+     * @param tileW
+     * @param tileH
+     */
+    public AnimatedLayer(int x, int y, int tileW, int tileH) {
+        super(x, y);
 
-	private void setFrame(int frame) {
-				
-		if(animateHorizontally) {
-			setXImage(needleX+tileW*frame);
-		} else {
-			setYImage(needleY+tileH*frame);
-		}
+        this.tileW = tileW;
+        this.tileH = tileH;
+    }
 
-	}
-	
-	@Override
-	public int utilWidth() {
-		return tileW;
-	}
-	
-	@Override
-	public int utilHeight() {
-		return tileH;
-	}
-	
-	/**
-	 * 
-	 * @param stopped
-	 */
-	public void setStopped(boolean stopped) {
-		this.stopped = stopped;
-	}
+    public void restartAnimation() {
+        stopped = false;
+        resetAnimation();
+    }
 
-	public boolean isStopped() {
-		return stopped;
-	}
+    public void resetAnimation() {
+        xImage = needleX;
+        yImage = needleY;
+        currentFrame = 0;
+    }
+
+    public void setAnimateHorizontally(boolean animateHorizontally) {
+        this.animateHorizontally = animateHorizontally;
+    }
+
+    public int getTileW() {
+        return tileW;
+    }
+
+    public int getTileH() {
+        return tileH;
+    }
+
+    /**
+     * @param tileW
+     */
+    public void setTileW(int tileW) {
+        this.tileW = tileW;
+    }
+
+    /**
+     * @param tileH
+     */
+    public void setTileH(int tileH) {
+        this.tileH = tileH;
+    }
+
+    /**
+     * @param tileW
+     * @param tileH
+     */
+    public void setTileCoordinates(int tileW, int tileH) {
+        setTileW(tileW);
+        setTileH(tileH);
+    }
+
+    public void animateWithFrame(int frame) {
+        if (this.currentFrame != frame) {
+            notifyFrameChangeListener(0, frame);
+        }
+
+        setFrame(frame);
+
+        if (frame == frames - 1) {
+            notifyAnimationFinishListener(0);
+        }
+    }
+
+    public void animate(long now) {
+
+        if (stopped) {
+
+            startedAt = now;
+            changedAt = now;
+
+            restartAnimation();
+        }
+
+        if (now >= changedAt + speed) {
+
+            changedAt = now;
+
+            boolean hasNextFrame = nextFrame();
+
+            notifyFrameChangeListener(now, currentFrame);
+
+            if (!hasNextFrame) {
+                notifyAnimationFinishListener(now);
+            }
+
+        }
+
+    }
+
+    //Notify Listener about the end of animation
+    protected void notifyAnimationFinishListener(long now) {
+        onAnimationFinishListener.onAnimationFinish(now);
+    }
+
+    //Notify Listener about the frame change
+    protected void notifyFrameChangeListener(long now, int frame) {
+        onFrameChangeListener.onFrameChange(now, frame);
+    }
+
+    public void animate() {
+        nextFrame();
+        stopped = false;
+    }
+
+    public void stopAnimation() {
+        stopped = true;
+    }
+
+    public void animateOnce() {
+        once = true;
+        stopped = false;
+        lockOnce = false;
+
+        currentFrame = 0;
+        setFrame(currentFrame);
+    }
+
+    public boolean nextFrame() {
+
+        boolean hasNextFrame = true;
+
+        if ((currentFrame < frames - 1) && (currentFrame >= 0)) {
+
+            currentFrame += inc;
+
+        } else {
+
+            if (once) {
+                lockOnce = true;
+                //stopped = true;
+                //setFrame(currentFrame);
+
+            } else {
+                currentFrame = 0;
+            }
+
+            hasNextFrame = false;
+
+        }
+
+        if (!stopped) {
+            setFrame(currentFrame);
+        }
+
+        return hasNextFrame;
+    }
+
+    private void setFrame(int frame) {
+
+        if (animateHorizontally) {
+            setXImage(needleX + tileW * frame);
+        } else {
+            setYImage(needleY + tileH * frame);
+        }
+
+    }
+
+    @Override
+    public int utilWidth() {
+        return tileW;
+    }
+
+    @Override
+    public int utilHeight() {
+        return tileH;
+    }
+
+    /**
+     * @param stopped
+     */
+    public void setStopped(boolean stopped) {
+        this.stopped = stopped;
+    }
+
+    public boolean isStopped() {
+        return stopped;
+    }
 
 
-	/**
-	 * Set Number of Frames
-	 * 
-	 * @param frames
-	 */
-	public void setFrames(int frames) {
-		this.frames = frames;
-	}
+    /**
+     * Set Number of Frames
+     *
+     * @param frames
+     */
+    public void setFrames(int frames) {
+        this.frames = frames;
+    }
 
-	public int getFrames() {
-		return frames;
-	}
-	
-	public int getCurrentFrame() {
-		return currentFrame;
-	}
+    public int getFrames() {
+        return frames;
+    }
 
-	public boolean getAnimateHorizontally() {
-		return animateHorizontally;
-	}
+    public int getCurrentFrame() {
+        return currentFrame;
+    }
 
-	public void setLockOnce(boolean lockOnce) {
-		this.lockOnce = lockOnce;
-	}
+    public boolean getAnimateHorizontally() {
+        return animateHorizontally;
+    }
 
-	public int getSpeed() {
-		return speed;
-	}
+    public void setLockOnce(boolean lockOnce) {
+        this.lockOnce = lockOnce;
+    }
 
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
+    public int getSpeed() {
+        return speed;
+    }
 
-	public int getNeedleX() {
-		return needleX;
-	}
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 
-	public void setNeedleX(int needleX) {
-		this.needleX = needleX;
-	}
+    public int getNeedleX() {
+        return needleX;
+    }
 
-	public int getNeedleY() {
-		return needleY;
-	}
+    public void setNeedleX(int needleX) {
+        this.needleX = needleX;
+    }
 
-	public void setNeedleY(int needleY) {
-		this.needleY = needleY;
-	}
+    public int getNeedleY() {
+        return needleY;
+    }
 
-	public OnAnimationFinishListener getListener() {
-		return onAnimationFinishListener;
-	}
+    public void setNeedleY(int needleY) {
+        this.needleY = needleY;
+    }
 
-	public void setOnAnimationFinishListener(OnAnimationFinishListener onAnimationFinishListener) {
-		this.onAnimationFinishListener = onAnimationFinishListener;
-	}
-	
-	public void setOnFrameChangeListener(OnFrameChangeListener onFrameChangeListener) {
-		this.onFrameChangeListener = onFrameChangeListener;
-	}
-		
+    public OnAnimationFinishListener getListener() {
+        return onAnimationFinishListener;
+    }
+
+    public void setOnAnimationFinishListener(OnAnimationFinishListener onAnimationFinishListener) {
+        this.onAnimationFinishListener = onAnimationFinishListener;
+    }
+
+    public void setOnFrameChangeListener(OnFrameChangeListener onFrameChangeListener) {
+        this.onFrameChangeListener = onFrameChangeListener;
+    }
+
 }
