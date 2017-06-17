b661c635cd1f6815b735ca5754661d397a1acac8
Yuri Pourre <yuripourre@gmail.com>
2017-06-16 23:12:35 -0300

    Remove awt dependencies from GenericLoadApplication

--- src/main/java/br/com/etyllica/commons/context/load/GenericLoadApplication.java
+++ src/main/java/br/com/etyllica/commons/context/load/GenericLoadApplication.java
@@ -1,58 +1,55 @@
 package br.com.etyllica.commons.context.load;
 
-import java.awt.Color;
-
 import br.com.etyllica.core.graphics.Graphics;
 
 /**
- * 
  * @author yuripourre
- *
  */
 
 public class GenericLoadApplication extends DefaultLoadApplication {
-	
-	protected float fill = 0;
-	
-	public GenericLoadApplication(int x, int y, int w, int h) {
-		super(x, y, w, h);
-	}
-	
-	private float rectW = w*2/3;
-	private float rectX = w/2-rectW/2;
-	private float rectY = h/2+100;
-	private float rectH = 32;
 
-	private Color backgroundColor = new Color(0x00,0xcc,0xff);
-	
-	@Override
-	public void load() {
+    protected float fill = 0;
 
-	}
-	
-	@Override
-	public void onChangeText(String phrase) {
-		this.phrase = phrase;
-	}
+    public GenericLoadApplication(int x, int y, int w, int h) {
+        super(x, y, w, h);
+    }
 
-	@Override
-	public void onChangeLoad(float load) {
-		this.percent = Integer.toString((int)load)+"%";
-		this.fill = load;
-	}
-	
-	@Override
-	public void draw(Graphics g) {
-		g.setColor(backgroundColor);
-		g.fillRect(x, y, w, h);
-		
-		g.setColor(Color.WHITE);
-		g.drawStringShadowX(phrase, 280-y);
+    private int rectW = w * 2 / 3;
+    private int rectX = w / 2 - rectW / 2;
+    private int rectY = h / 2 + 100;
+    private int rectH = 32;
 
-		g.drawRect(rectX, rectY, rectW, rectH);
-		g.fillRect(rectX+2, rectY+2, (int)((rectW*fill)/100)-3, rectH-3);
-		
-		g.drawStringShadow(percent, rectX, rectY, rectW, rectH, Color.BLACK);
-	}
-	
+    private int foregroundColor = -1;
+    private int backgroundColor = -11472993;
+
+    @Override
+    public void load() {
+
+    }
+
+    @Override
+    public void onChangeText(String phrase) {
+        this.phrase = phrase;
+    }
+
+    @Override
+    public void onChangeLoad(float load) {
+        this.percent = Integer.toString((int) load) + "%";
+        this.fill = load;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(backgroundColor);
+        g.fillRect(x, y, w, h);
+
+        g.setColor(foregroundColor);
+        g.drawStringX(phrase, 280 - y);
+
+        g.drawRect(rectX, rectY, rectW, rectH);
+        g.fillRect(rectX + 2, rectY + 2, (int) ((rectW * fill) / 100) - 3, rectH - 3);
+
+        g.drawString(percent, rectX, rectY, rectW, rectH);
+    }
+
 }
