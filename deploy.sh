cfaf7a39d77fa37bd2a1a12cd83bac426ee32347
Yuri Pourre <yuripourre@gmail.com>
2017-06-22 22:31:48 -0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,17 @@
 
 VERSION=$1
 
-if [ -z "$VERSION" ]
-  echo "Command expects a version as: sh deploy 1.0.0"
+if [ -z "$1" ]
 then
+  echo 'Command expects a version as in: sh deploy 1.0.0'
+else
   # Edit build.xml
   sed -i -e 's/name="version" value="[^"]*"/name="version" value="'"$VERSION"'"/g' build.xml
   # Edit maven/pom.xml
   sed -i -E "s|(<version>)(.*)(</version>)|\1$VERSION\3|" maven/pom.xml
   # Run ant stage
   ant stage
+  echo 'Access: https://oss.sonatype.org/#stagingRepositories'
 fi
 
 
