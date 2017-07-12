f22df39968c8b664923b493f0b5d461e460c23e1
Yuri Pourre <yuripourre@gmail.com>
2017-07-12 12:04:56 -0300

    Add more tests to Layer

--- src/test/java/br/com/etyllica/layer/LayerTest.java
+++ src/test/java/br/com/etyllica/layer/LayerTest.java
@@ -6,25 +6,46 @@ import org.junit.Test;
 
 public class LayerTest {
 
-	private Layer layer;
-	
-	@Before
-	public void setUp() {
-		
-		layer = new Layer(0, 0, 32, 32);
-		
-		layer.setVisible(false);
-				
-	}
-	
-	@Test
-	public void testSwapVisible() {
-		
-		layer.setVisible(false);
-		layer.swapVisible();
-		
-		Assert.assertTrue(layer.isVisible());
-		
-	}
-		
+    private Layer layer;
+
+    @Before
+    public void setUp() {
+        layer = new Layer(0, 1, 31, 32);
+        layer.setOpacity(22);
+        layer.setOrigin(33,34);
+        layer.setVisible(false);
+    }
+
+    @Test
+    public void testGetUtilMeasures() {
+        Assert.assertEquals(31, layer.utilWidth());
+        Assert.assertEquals(32, layer.utilHeight());
+
+        layer.setW(200);
+        Assert.assertEquals(200, layer.utilWidth());
+    }
+
+    @Test
+    public void testSwapVisible() {
+        layer.setVisible(false);
+        layer.swapVisible();
+
+        Assert.assertTrue(layer.isVisible());
+    }
+
+    @Test
+    public void testCopyLayer() {
+        Layer l = new Layer();
+        l.copy(layer);
+
+        Assert.assertEquals(0, l.getX());
+        Assert.assertEquals(1, l.getY());
+        Assert.assertEquals(31, l.getW());
+        Assert.assertEquals(32, l.getH());
+        Assert.assertEquals(33, l.getOriginX(), 0);
+        Assert.assertEquals(34, l.getOriginY(), 0);
+        Assert.assertEquals(22, l.getOpacity());
+
+    }
+
 }
